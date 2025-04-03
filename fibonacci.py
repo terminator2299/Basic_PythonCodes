80319a63413742a62ce0ce6cf2be762ce0a5710b
Bhavya khandelwal <bhavya.code18@gmail.com>
2025-04-03 18:56:59 +0530

    Day 2nd

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci_iterative(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+# Example usage
+print(fibonacci_iterative(6))  # Output: 8
---
+++ gcd.py
@@ -0,0 +1,10 @@
+#Recursive Approach
+
+
+def gcd(a,b):
+    if b==0:
+        return a
+    return gcd(b , a%b)
+
+
+print(gcd(48,18))
