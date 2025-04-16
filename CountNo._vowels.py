a02353f0b07c9284ed258ac4fc96c0d7f02970f7
Bhavya khandelwal <bhavya.code18@gmail.com>
2025-04-16 17:35:39 +0530

    Day-3rd

---
+++ CountNo._vowels.py
@@ -0,0 +1,10 @@
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+
+s = input("Enter a string: ")
+print("Number of vowels:", count_vowels(s))
